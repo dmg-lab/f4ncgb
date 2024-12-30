e0f23ed571e0ed599011bdfea943def0b242c5d4
Clemens Hofstadler <clemens.hofstadler@liwest.at>
2024-12-30 16:03:31 +0100

    Remove ahocorasick - no longer needed

--- aho_corasick/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/clemenshofstadler/Desktop/Work/Code/kommunopp/aho_corasick
-# Build directory: /Users/clemenshofstadler/Desktop/Work/Code/kommunopp/aho_corasick
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
--- aho_corasick/src/aho_corasick/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/clemenshofstadler/Desktop/Work/Code/kommunopp/aho_corasick/src/aho_corasick
-# Build directory: /Users/clemenshofstadler/Desktop/Work/Code/kommunopp/aho_corasick/src/aho_corasick
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
