8d1a01740b2a91ea61cf9753b30e7c2681395750
Max Heisinger <mail@maxheisinger.at>
2025-01-20 13:54:37 +0100

    Small additions fixes for ambiguity.hpp
    These additional keywords express intent and may lead to some
    optimization, although they also make it clearer to read.
    
    The members may not be declared const, as sorting elements of the
    ambiguity type in a vector ceases to be possible then.

--- ambiguity.hpp
+++ ambiguity.hpp
@@ -8,8 +8,7 @@
 namespace kommunopp {
 
 template<typename I>
-size_t
-compute_hash(const std::array<I, 7>& values) {
+size_t inline compute_hash(const std::array<I, 7>& values) noexcept {
   size_t seed = 0;
   for(I value : values)
     boost::hash_combine(seed, value);
@@ -22,27 +21,20 @@ struct ambiguity {
   std::array<I, 7> values;
   size_t hash_;
 
-  ambiguity(I d, I i, I j, I ai, I ci, I aj, I cj) {
-    values[0] = d;
-    values[1] = i;
-    values[2] = j;
-    values[3] = ai;
-    values[4] = ci;
-    values[5] = aj;
-    values[6] = cj;
-    hash_ = compute_hash(values);
-  }
+  inline ambiguity(I d, I i, I j, I ai, I ci, I aj, I cj)
+    : values{ d, i, j, ai, ci, aj, cj }
+    , hash_(compute_hash(values)) {}
 
-  inline I degree() const { return values[0]; }
-  inline I i() const { return values[1]; }
-  inline I j() const { return values[2]; }
-  inline I ai() const { return values[3]; }
-  inline I ci() const { return values[4]; }
-  inline I aj() const { return values[5]; }
-  inline I cj() const { return values[6]; }
+  inline I degree() const noexcept { return values[0]; }
+  inline I i() const noexcept { return values[1]; }
+  inline I j() const noexcept { return values[2]; }
+  inline I ai() const noexcept { return values[3]; }
+  inline I ci() const noexcept { return values[4]; }
+  inline I aj() const noexcept { return values[5]; }
+  inline I cj() const noexcept { return values[6]; }
 
   // Equality operator for unordered_set
-  bool operator==(const ambiguity& other) const {
+  inline bool operator==(const ambiguity& other) const noexcept {
     return (hash_ == other.hash_) and (values == other.values);
   }
 };
@@ -50,19 +42,21 @@ struct ambiguity {
 // Custom hash function
 template<typename I>
 struct ambiguity_hash {
-  size_t operator()(const ambiguity<I>& a) const { return a.hash_; }
+  inline size_t operator()(const ambiguity<I>& a) const noexcept {
+    return a.hash_;
+  }
 };
 
 template<class T, std::size_t N, std::size_t M>
-constexpr bool
-startswith(std::span<T, N> data, std::span<T, M> prefix) {
+constexpr inline bool
+startswith(std::span<T, N> data, std::span<T, M> prefix) noexcept {
   return data.size() >= prefix.size()
          && std::equal(prefix.begin(), prefix.end(), data.begin());
 }
 
 template<class T, std::size_t N, std::size_t M>
-constexpr bool
-endswith(std::span<T, N> data, std::span<T, M> suffix) {
+constexpr inline bool
+endswith(std::span<T, N> data, std::span<T, M> suffix) noexcept {
   return data.size() >= suffix.size()
          && std::equal(data.end() - static_cast<long>(suffix.size()),
                        data.end(),
