49a9fca1af4b016235ea55bf462ee3878b31edf2
Max Heisinger <mail@maxheisinger.at>
2025-04-08 14:38:49 +0200

    Disable prints again

--- linear_algebra.cpp
+++ linear_algebra.cpp
@@ -526,7 +526,7 @@ gauss_elim(uint32_mat_t mat, nmod_t mod, size_t num_threads, bool* trace) {
 
     // reduce row with all already known pivots
 
-    std::ios::sync_with_stdio(false);
+    // std::ios::sync_with_stdio(false);
     
     auto task = [r, &mat, &atomic_pivots, &trace, p, p2, &mod]() {
       KOMMUNOPP_TIME(elim_task_cpu);
@@ -540,11 +540,11 @@ gauss_elim(uint32_mat_t mat, nmod_t mod, size_t num_threads, bool* trace) {
         size_t i = row->indices[0];
         while(i < mat->ncol) {
 
-          std::cout << "{";
-          for(int idx = 0; idx < mat->ncol; idx++)
-            if (buffer_local[idx] != 0)
-              std::cout << idx << " ";
-          std::cout << "\n";
+          // std::cout << "{";
+          // for(int idx = 0; idx < mat->ncol; idx++)
+          //   if (buffer_local[idx] != 0)
+          //     std::cout << idx << " ";
+          // std::cout << "\n";
           
           assert(buffer_local[i] > 0);
           // v must be smaller than 2^2b, i.e. 2^62
