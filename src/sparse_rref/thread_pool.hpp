6dc7f6a75fcc841def7480dca3b79fec15d3217c
Benjamin Lorenz <lorenz@math.tu-berlin.de>
2025-06-16 10:43:09 +0200

    freebsd also defines major / minor macros

--- src/sparse_rref/thread_pool.hpp
+++ src/sparse_rref/thread_pool.hpp
@@ -96,7 +96,7 @@ import std;
     #endif
 #endif
 
-#if defined(__linux__)
+#if defined(__linux__) || defined(__FreeBSD__)
     // On Linux, <sys/sysmacros.h> defines macros called `major` and `minor`. We undefine them here so the `version` struct can work.
     #ifdef major
         #undef major
