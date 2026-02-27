e49419b3364856b443de29dc6eb76ac8757f5356
Clemens Hofstadler <clemens.hofstadler@liwest.at>
2026-02-27 01:03:33 +0100

    coeff sources

---
+++ src/coeff.hpp
@@ -0,0 +1,108 @@
+#pragma once
+
+#include <boost/multiprecision/gmp.hpp>
+
+#include <flint/fmpz.h>
+namespace f4ncgb {
+
+inline void
+fmpz_cleanup(fmpz* arr, size_t len) {
+  for(size_t i = 0; i < len; i++)
+    fmpz_clear(arr + i);
+  delete[] arr;
+}
+
+struct coeff {
+  fmpz_t value;
+
+  coeff() { fmpz_init(value); }
+  ~coeff() { fmpz_clear(value); }
+
+  coeff(const coeff& other) {
+    fmpz_init(value);
+    fmpz_set(value, other.value);
+  }
+
+  coeff(coeff&& other) noexcept {
+    fmpz_init(value);
+    fmpz_swap(value, other.value);
+  }
+
+  // Leaves x in valid but unspecified state
+  explicit coeff(fmpz_t&& x) {
+    fmpz_init(value);
+    fmpz_swap(value, x);
+  }
+
+  coeff(long x) {
+    fmpz_init(value);
+    fmpz_set_si(value, x);
+  }
+
+  explicit coeff(const mpz_t x) {
+    fmpz_init(value);
+    fmpz_set_mpz(value, x);
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, const coeff& c) {
+    char* s = fmpz_get_str(nullptr, 10, c.value);
+    os << s;
+    flint_free(s);
+    return os;
+  }
+
+  bool operator==(const coeff& other) const {
+    return fmpz_equal(value, other.value) != 0;
+  }
+
+  bool operator!=(const coeff& other) const { return !(*this == other); }
+
+  coeff& operator=(const coeff& other) {
+    if(this != &other)
+      fmpz_set(value, other.value);
+    return *this;
+  }
+
+  coeff& operator=(coeff&& other) noexcept {
+    if(this != &other)
+      fmpz_swap(value, other.value);
+    return *this;
+  }
+
+  coeff& operator*=(const coeff& other) {
+    fmpz_mul(value, value, other.value);
+    return *this;
+  }
+
+  coeff& operator/=(long b) {
+    fmpz_t tmp;
+    fmpz_init(tmp);
+    fmpz_set_si(tmp, b);
+
+    fmpz_divexact(value, value, tmp);
+
+    fmpz_clear(tmp);
+    return *this;
+  }
+
+  int sign() const { return fmpz_sgn(value); }
+
+  bool is_zero() const { return fmpz_is_zero(value); }
+
+  bool is_unit() const { return fmpz_is_pm1(value); }
+
+  coeff abs() const {
+    coeff r;
+    fmpz_abs(r.value, value);
+    return r;
+  }
+
+  void lcm_inplace(coeff& b) { fmpz_lcm(value, value, b.value); }
+
+  void lcm_inplace(long b) {
+    coeff tmp(b);
+    lcm_inplace(tmp);
+  }
+};
+
+}
